a4afec3e0c0dd4d83ce60663dde9b555b679555b
Murilooaguiar <146784737+Murilooaguiar@users.noreply.github.com>
2023-10-25 11:42:51 -0300

    atividade23

---
+++ atividade23.py
@@ -0,0 +1,6 @@
+# Exercício Python 23: Faça um programa que calcule a soma entre todos os números que são múltiplos de três
+# e que se encontram no intervalo de 1 até 500.
+
+for multiplode3 in  range (500):
+    if multiplode3 %3==0:
+        print (multiplode3)
